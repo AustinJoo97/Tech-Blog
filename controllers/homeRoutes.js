fcb8f264765bdf93f84d530042bc6677110fba54
AustinJoo97 <austinjoo97@gmail.com>
2021-05-17 14:18:30 -0400

    added blogPosts handlebar; added home route which should render all previous posts to home

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, BlogPost } = require('../models');
 const ishAuthorized = require('../utils/auth');
 
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const blogPostData = await BlogPost.findAll();
+  
+      // Serialize data so the template can read it
+      const blogPostss = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        blogPosts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
