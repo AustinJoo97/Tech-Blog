ed8dea729a67a3582cf1078cc1d0a12b3ac23226
AustinJoo97 <austinjoo97@gmail.com>
2021-05-17 12:48:54 -0400

    added utility function js files; added to server code

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
