cc30da72cea73562f2d75b856b146884d74ca7a2
AustinJoo97 <austinjoo97@gmail.com>
2021-05-17 13:46:59 -0400

    added initial controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const ishAuthorized = require('../utils/auth');
+
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
